8c1091ddd2088fbfa003f0e12191548b7be80080
Stevtm <steventmaddison@outlook.com>
2021-07-11 16:08:52 -0400

    style footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,32 @@
 import React from "react";
 
 // import icons from assets folder
-import GitHubIcon from "../../assets/images/GitHub.png";
-import InstagramIcon from "../../assets/images/Instagram.png";
-import LinkedinIcon from "../../assets/images/LinkedIn.png";
+import { FaGithubSquare, FaLinkedin, FaInstagramSquare } from "react-icons/fa";
 
 // import styled components
-import { FooterContainer, Link, Icon } from "./styles";
+import { FooterContainer, Link, Links, Copyright } from "./styles";
 
 const Footer = () => {
 	return (
 		<FooterContainer>
-			<Link href="https://github.com/Stevtm" target="_blank">
-				<Icon src={GitHubIcon}></Icon>
-			</Link>
-			<Link href="https://www.linkedin.com/in/stevenmaddison/" target="_blank">
-				<Icon src={InstagramIcon}></Icon>
-			</Link>
-			<Link href="https://www.instagram.com/steeeeeeeeeeeeev/" target="_blank">
-				<Icon src={LinkedinIcon}></Icon>
-			</Link>
+			<Links>
+				<Link href="https://github.com/Stevtm" target="_blank">
+					<FaGithubSquare></FaGithubSquare>
+				</Link>
+				<Link
+					href="https://www.linkedin.com/in/stevenmaddison/"
+					target="_blank"
+				>
+					<FaLinkedin></FaLinkedin>
+				</Link>
+				<Link
+					href="https://www.instagram.com/steeeeeeeeeeeeev/"
+					target="_blank"
+				>
+					<FaInstagramSquare></FaInstagramSquare>
+				</Link>
+			</Links>
+			<Copyright>© Steven Maddison 2021</Copyright>
 		</FooterContainer>
 	);
 };
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,17 +2,46 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.div`
 	/* box model */
-	padding: 5px 10px;
-	margin: 20px 0;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	padding: 15px 10px;
 	width: 100%;
 
 	/* visual decorations */
 	background-color: ${({ theme }) => theme.secondary};
 `;
 
-export const Link = styled.a``;
-
-export const Icon = styled.img`
+export const Links = styled.div`
 	/* box model */
-	width: 35px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	padding: 0 0;
+`;
+
+export const Link = styled.a`
+	/* box model */
+	padding: 0 10px;
+
+	/* typography */
+	font-size: 2rem;
+
+	/* visual decorations */
+
+	svg {
+		/* visual decorations */
+		color: white;
+	}
+`;
+
+export const Copyright = styled.p`
+	/* box model */
+	margin: 0;
+	padding: 0;
+
+	/* typography */
+	color: white;
+
+	/* visual decorations */
 `;
