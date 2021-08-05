86c1169776ac64d1a3c899b0ba82769ed91e2aa2
Stevtm <steventmaddison@outlook.com>
2021-08-04 22:09:26 -0400

    redefine style colours for consistent shade

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -16,10 +16,6 @@ export const StyledMenu = styled.nav`
 	transition: transform 0.3s ease-in-out;
 	margin: 0 auto;
 
-	/* @media (max-width: ${({ theme }) => theme.mobile}) {
-		width: 100%;
-	} */
-
 	a {
 		font-size: 2rem;
 		text-transform: uppercase;
@@ -29,14 +25,5 @@ export const StyledMenu = styled.nav`
 		color: ${({ theme }) => theme.secondary};
 		text-decoration: none;
 		transition: color 0.3s linear;
-
-		/* @media (max-width: ${({ theme }) => theme.mobile}) {
-			font-size: 1.5rem;
-			text-align: center;
-		} */
-
-		/* &:hover {
-			color: ${({ theme }) => theme.primaryHover};
-		} */
 	}
 `;
--- src/global.js
+++ src/global.js
@@ -14,7 +14,6 @@ export const GlobalStyles = createGlobalStyle`
     }
 
     body {
-        /* background-color: ${({ theme }) => theme.primary}; */
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
         height: 100vh;
         text-rendering: optimizeLegibility;
